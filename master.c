a3754ac7ffc89d88436148a0c510e7c87ff23604
cod0216 <cod0216@nate.com>
2022-06-15 18:57:56 +0900

    last fix master.c file in master branch at local2 folder

--- master.c
+++ master.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 100
-#define MAX 10
+#define MAX_SIZE 100
+#define ELEMENT 100
+
+typedef struct node{
+	int key;
+	int index[MAX_SIZE];
+} node;
+
+
 int main()
 {
 	printf("sample file in master branch\n");
