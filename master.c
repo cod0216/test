a3fbe9c86805421e572a3118f33fd68353a649a8
cod0216 <cod0216@nate.com>
2022-06-15 19:03:41 +0900

    fix master.c file in master branch at local1 folder

--- master.c
+++ master.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#define SIZE 100
-#define MAX 10
 
 int main()
 {
 	int a = time(rand(NULL));
 	printf("%d " ,a);
 
-	printf("sample file in master branch\n");
+	printf("never drinked yesterday real! ");
 	return 0;
 }
